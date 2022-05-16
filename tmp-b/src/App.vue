2bd1d7249c8f7db51f25e483dc0a30cfb3b9b151
Mark <mcummins@educatemagis.org>
2022-05-16 20:38:49 +0100

    first commit

---
+++ tmp-b/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js Appelication.."/>
+</template>
+
+<script>
+// import HelloWorld from 'tmp'
+import HelloWorld from '../../tmp/dist/tmp.umd.js' 
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
